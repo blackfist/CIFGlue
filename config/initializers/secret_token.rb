58ba1cc744f15c9010fb272afce6f19b1642b150
Kevin Thompson <kevin@kevin-linux.(none)>
2012-06-20 09:29:32 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cif::Application.config.secret_token = 'b1ecb018ace624977717fe12ebb37fd09a561728ecc166f3763379f66b4328bbb0de1b36f915668a7d72934f013c4a5b716797617cc887fb11a4216153d72042'
