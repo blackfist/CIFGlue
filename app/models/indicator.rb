9acf6ba0930d546b44de2ed327a62b6ca93f2e4c
Kevin Thompson <kevin.david.thompson@gmail.com>
2012-07-30 16:24:47 -0400

    changed Indicator.by_type to a class method instead of a scope.

--- app/models/indicator.rb
+++ app/models/indicator.rb
@@ -2,10 +2,18 @@ class Indicator < ActiveRecord::Base
   attr_accessible :analyst, :case, :content, :description, :friendly_name
   validates :content, :description, :analyst, :presence => true
   scope :others, where(:type => nil)
-  scope :by_type, lambda { |type| where(:type => type) }
-      
 
   def friendly_name
       "indicator"
+  end
+
+  def self.by_type(type)
+      if type.nil?
+          Indicator.all
+      elsif type.downcase == "other"
+          Indicator.others
+      else
+          Indicator.where(:type => type)
+      end
   end
 end
