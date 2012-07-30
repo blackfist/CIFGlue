37e3e5da59b82e0fc326a2ccdca729fb9b9d1ec0
Kevin Thompson <kevin.david.thompson@gmail.com>
2012-07-30 16:19:53 -0400

    added a couple named scopes to the indicator model

--- app/models/indicator.rb
+++ app/models/indicator.rb
@@ -1,7 +1,9 @@
 class Indicator < ActiveRecord::Base
   attr_accessible :analyst, :case, :content, :description, :friendly_name
-
   validates :content, :description, :analyst, :presence => true
+  scope :others, where(:type => nil)
+  scope :by_type, lambda { |type| where(:type => type) }
+      
 
   def friendly_name
       "indicator"
