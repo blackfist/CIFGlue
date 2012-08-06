b4473d2b36decce0f1a6d978d18f3e54597377db
Kevin Thompson <kevin.david.thompson@gmail.com>
2012-08-06 17:38:08 -0400

    trying to fix a problem with dropdown.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require dataTables/jquery.dataTables
-//= require dataTables/jquery.dataTables.bootstrap
 //= require_tree .
