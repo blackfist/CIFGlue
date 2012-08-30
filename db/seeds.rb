b5d56d5bb8d88546e84c78b95a6516fa51907d4d
Kevin Thompson <kevin.david.thompson@gmail.com>
2012-08-29 22:58:29 -0400

    added seed data for privacy tags.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+["Red","Amber","Green"].each do |tag|
+    PrivacyTag.find_or_create_by_content(tag)
+end
